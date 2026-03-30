becb48723a32659b144db4b8f68410245164a81c
Mike <pickysaurus@googlemail.com>
2026-03-30 08:30:53 +0100

    Fixed file name on action upload

--- Mod Organizer Plugin Browser/constants.py
+++ Mod Organizer Plugin Browser/constants.py
@@ -2,5 +2,5 @@ import mobase # type: ignore
 
 MAIN_PLUGIN_NAME = "Plugin Browser"
 UPDATE_PLUGIN_NAME = "Plugin Browser Update Notifications"
-VERSION = mobase.VersionInfo(1, 0, 1)
+VERSION = mobase.VersionInfo(1, 0, 2)
 AUTHOR = "Pickysaurus"
